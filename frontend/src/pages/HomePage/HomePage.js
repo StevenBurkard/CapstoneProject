688aa3503cf132158b481a96f0e321d21a2db85d
Burkard920 Burkard <stevenburkard24@yahoo.com>
2023-08-16 08:35:35 -0500

    feat: logged in football fan now can see their name, username, and total amount of their account balance which shows either positive or negative

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -9,33 +9,45 @@ const HomePage = () => {
   // The "token" value is the JWT token that you will send in the header of any request requiring authentication
   //TODO: Add an AddCars Page to add a car for a logged in user's garage
   const [user, token] = useAuth();
-  const [cars, setCars] = useState([]);
+  const [bets, setBets] = useState([]);
+  const [totalAmount, setTotalAmount] = useState(0);
 
   useEffect(() => {
-    const fetchCars = async () => {
+    const fetchBets = async () => {
       try {
-        let response = await axios.get("http://127.0.0.1:5000/api/user_cars", {
+        let response = await axios.get("http://127.0.0.1:5000/api/user_bets", {
           headers: {
             Authorization: "Bearer " + token,
           },
         });
-        setCars(response.data);
+        setBets(response.data);
       } catch (error) {
         console.log(error.response.data);
       }
     };
-    fetchCars();
+    fetchBets();
   }, [token]);
+
+  useEffect(() => {
+    const calculateTotalAmount = () => {
+      let total = 0;
+      bets.forEach(bet => {
+        if (bet.did_user_win) {
+          total += bet.unit_amount;
+        } else {
+          total -= bet.unit_amount;
+        }
+      });
+      setTotalAmount(total);
+    };
+    calculateTotalAmount();
+  }, [bets]);
+  
   return (
     <div className="container">
-      {console.log(user)}
-      <h1>{user.username}'s Home Page</h1>
-      {cars &&
-        cars.map((car) => (
-          <p key={car.id}>
-            {car.year} {car.model} {car.make}
-          </p>
-        ))}
+      <h1>{user.first_name} {user.last_name}'s Profile</h1>
+      <h3>Username: {user.username}</h3>
+      <h3>Total Amount: {totalAmount > 0 ? `+${totalAmount}` : totalAmount}</h3>
     </div>
   );
 };
